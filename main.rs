acee3a074b86e9ab29b874f5953072072e151702
Rylen Latosky <rylen@archlinux-nvidia.rdlinux>
2024-01-08 07:22:42 -0500

    01-08-24 Update

--- main.rs
+++ main.rs
@@ -1,14 +1,9 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashMap;
-
-use structures::{Node, Switch, Chunk};
+use substring::Substring;
 
 // Clap used for arguments
-use clap::{arg, command, value_parser, ArgAction, Command};
-use std::path::PathBuf;
-
 /*
     Some Information:
     ->
@@ -29,46 +24,175 @@ struct Args {
     output: String,
 }
 
-fn main() {
-    let args = Args::parse();
-    println!("output: {:?}", cli.output);
+// Nodes include [H] AND [S] - anything below a Switch
+struct Node {
+    hostname: String,
+    uid: String,
+    portnum: String,
+    link_speed: String,
+}
 
-    let file = get_paragraphs("./ibnetdiscover2023-01-02-18-29-2.txt");
-
-    for paragraph in file.clone() {
-        let Switch = build_switch(&paragraph);
-        let Node = build_node(&paragraph);
-        let Chunk = build_chunk(&paragraph);
+impl Node {
+    pub fn build_node(hostname: String, uid: String, portnum: String, link_speed: String) -> Node {
+        return Node {
+            hostname: hostname,
+            uid: uid,
+            portnum: portnum,
+            link_speed: link_speed,
+        }
     }
 }
 
-/* get_switches: read from paragraph */
-fn get_switches(paragraph: Vec<String>) {
+struct Switch {
+    guid: String,
+    model: String,
+    devices: Vec<Node>,
+    link_speeds: Vec<String>,
+}
+
+impl Switch {
+    pub fn build_switch(paragraph: &Vec<String>) -> Switch {
+        return Switch {
+            guid: extract_switch_guid(paragraph),
+            model: extract_switch_model(paragraph),
+            devices: extract_switch_devices(paragraph),
+            link_speeds: extract_switch_link_speeds(paragraph),
+        }
+    }
+
+}
+
+fn extract_switch_guid(paragraph: &Vec<String>) -> String {
+    let my_paragraph = paragraph.clone();
+    let mut guid = "<None>";
+    for line in my_paragraph {
+        if line.contains("switchguid") {
+            let start_index = line.find("(").unwrap_or(line.len());
+            let end_index = line.find(")").unwrap_or(line.len());
+            guid = line.substring(start_index, end_index);
+            println!("GUID SUBSTRING: {}", guid);
+            return guid.into();
+        } else {
+            return guid.into();
+        }
+    }
+    return guid.to_string();
+}
+
+fn extract_switch_model(paragraph: &Vec<String>) -> String {
+    let my_paragraph = paragraph.clone();
+    let mut model = "<None>";
+    for line in my_paragraph {
+        if line.contains("Switch") {
+            let mut start_index = 0;
+            let mut end_index = 0;
+            while line.find("#") > line.find("\"") {
+                start_index = line.find("\"").unwrap_or(line.len());
+                end_index = line.rfind("\"").unwrap_or(line.len());
+            }
+            model = line.clone().substring(start_index, end_index);
+            println!("MODEL SUBSTRING: {}", model);
+        }
+    }
+    return model.to_string();
+}
+// format as a HashMap(uid, portnum)
+// collect devices as a hashmap from '[]'
+fn extract_switch_devices(paragraph: &Vec<String>) -> Vec<Node> {
     let mut my_paragraph = paragraph.clone();
-    for line in paragraph {
-        
+    let mut found_uid;
+    let mut found_portnum;
+    let mut found_hostname;
+    let mut found_link_speeds;
+    let mut found_nodes: Vec<Node> = Vec::new();
+
+    for line in my_paragraph { // working with String
+        let mut start_index: usize;
+        let mut end_index: usize;
+        let device_code: String = line.substring(line.find("\"").unwrap_or(line.len()),line.find("\"").unwrap_or(line.len())+1).to_string();
+        let last_word = line.split(' ').last().unwrap();
+        if line.contains("[") {
+            start_index = line.find("(").unwrap_or(line.len());
+            end_index = line.find(")").unwrap_or(line.len());
+            found_uid = line.substring(start_index, end_index);
+
+            start_index = line.find("[").unwrap_or(line.len());
+            end_index = line.find("]").unwrap_or(line.len());
+
+            found_portnum = line.trim().substring(start_index, end_index);
+            while line.contains("#") < line.contains("\"") {
+                start_index = line.find("\"").unwrap_or(line.len());
+                end_index = line.rfind("\"").unwrap_or(line.len());
+            }
+            found_hostname = line.substring(start_index, end_index);
+            found_link_speeds = last_word.to_string();
+
+            found_nodes.push(Node::build_node(found_hostname.to_string(), found_portnum.to_string(),
+                found_uid.to_string(), found_link_speeds.to_string()));
+        }
+    }
+    return found_nodes;
+}
+
+fn extract_switch_link_speeds(paragraph: &Vec<String>) -> Vec<String> {
+    let mut my_paragraph = paragraph.clone();
+    let mut link_speeds: Vec<String> = Vec::new();
+
+    for line in my_paragraph {
+        let device_code: String = line.substring(line.find("\"").unwrap_or(line.len()),line.find("\"").unwrap_or(line.len())+1).to_string();
+        if line.contains("[") && device_code == "S" {
+            let last_word = line.split(' ').last().unwrap();
+            link_speeds.push(last_word.to_string());
+        }
+    }
+    return link_speeds;
+}
+
+// struct Chunk {
+//     nodes: vec!<vec!<Nodes>>,
+//     switches: vec!<Switch>,
+// }
+
+// impl Chunk {
+//     pub fn chunk_builder(paragraph: &Vec<String>) {
+//         Chunk {
+//             nodes: extract(paragraph),
+//             switches: get_total_switches(paragraph),
+//         }
+//     }
+// }
+
+fn main() {
+    let args = Args::parse();
+    println!("output: {:?}", args.output);
+
+    let file = get_paragraphs("./ibnetdiscover2023-01-02-18-29-2.txt");
+
+    for &paragraph in &file.clone() {
+        let Switch = Switch::build_switch(&paragraph);
+        //let Chunk = Chunk::build_chunk(&paragraph);
     }
 }
 
 /* get_paragraphs: start from read_lines and then save lines into a vector */
 fn get_paragraphs(filename: &str) -> Vec<Vec<String>> {
     // File ibnetdiscover2023-01-02-18-29-2.txt must exist in the current path
+    let mut txtfile = vec!();
+    let mut templines = vec!();
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
-        Vec<Vec<String>> file;
-        Vec<String> templines;
-        
+
         for line in lines.flatten() {
             if line.trim().is_empty() {
                 // New paragraph                                (Paragraph 1)        (Paragraph 2)
-                file.push(templines);   // File looks like: [ [line1, line2, ...], [line1, line2, ...], ... ]
+                txtfile.push(templines);   // File looks like: [ [line1, line2, ...], [line1, line2, ...], ... ]
                 templines.clear();
             } else {
                 templines.push(line);
             }
         }
     }
-    file.into();    // paragraph is removed after this function has finished running to preserve mem
+    return txtfile.into();    // paragraph is removed after this function has finished running to preserve mem
 }
 
 // The output is wrapped in a Result to allow matching on errors.
